4fb25e1a7c3814ec4a5945cb126f7bff06796cf4
Ugur Oezdemir <u.oezdemir@gmail.com>
2014-03-25 17:13:39 -0700

    Initial commit

---
+++ sign.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+
+
+from pybitcointools import *
+import json
+import argparse
+import sys
+
+#Signs each input of a raw transaction with the given private key
+def signRawTransaction(rawtx, privateKey, redeemScript): 
+    try:   
+        txj = deserialize(rawtx)
+        for i in range (0,len(txj['ins'])):
+            sig =multisign(rawtx,i,redeemScript,privateKey)
+            #print 'SIG:',sig
+            rawtx = apply_multisignatures(rawtx,i,redeemScript,sig)
+        return rawtx
+    except Exception, e:
+        print e
+        return None
+
+if __name__ == '__main__':
+
+    signedTX = None
+    if sys.stdin.isatty(): #terminal
+        parser = argparse.ArgumentParser(description='Coinjail transaction signing tool. ( www.coinjail.com )')
+        parser.add_argument("rawTX", help="The raw transaction string to be signed.")
+        parser.add_argument("redeemScript", help="Redeemscript for multisig transactions.")
+        parser.add_argument("privKey", help="Your private key.")
+
+        args = parser.parse_args()
+        signedTX = signRawTransaction(args.rawTX , args.privKey, args.redeemScript)
+
+    else: #pipe
+        try:
+            parser = argparse.ArgumentParser(description='Coinjail transaction signing tool. ( www.coinjail.com )')
+            parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+            parser.add_argument("privKey", help="Your private key.")
+            args = parser.parse_args()
+            jsonString = args.infile.read()
+            j = json.loads(jsonString)
+            rawtx = j.get('rawTransaction', '')
+            redeemScript = j.get('redeemScript','')
+            signedTX = signRawTransaction(rawtx , args.privKey, redeemScript)
+        except Exception,e:
+            pass
+    if signedTX is not None:
+        print signedTX
+    else:
+        print 'Error: Check arguments.'
+
+
